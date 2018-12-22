50e2e3c35055a8d066620503ec669b49d7167b2c
Sofia Rahimi <sofia.i.rahimi@gmail.com>
2018-12-22 00:33:40 +0000

    routes for editing and updating  articles, as well as model views created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,11 +4,46 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
     
+    def edit
+       @article = Article.find(params[:id]) 
+    end     
+    
+    
+     def show
+
+            @article = Article.find(params[:id])
+
+     end 
+     
+     
+      def update 
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was successfully updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end    
+      end    
+     
+     
+    
     def create
-      render plain: params[:article].inspect
-      @article = Article.new(article_params)
-      @article.save
-      redirect_to articles_show(@article)
+
+        @article = Article.new(article_params)
+
+    if @article.save
+
+        flash[:notice] = "Article was successfully created"
+
+        redirect_to article_path(@article)
+
+    else
+
+        render 'new'
+
+    end
+
     end
     
     private
@@ -16,6 +51,18 @@ class ArticlesController < ApplicationController
          def article_params 
             params.require(:article).permit(:title, :description)
          end     
-    
-    
-end    
+         
+   
+   
+   
+    # def update 
+    #     @article = Article.find(params[:id])
+    #     if @article.update
+    #         flash[:notice] = "Article was successfully updated"
+    #         redirect_to article_path(@article)
+    #     else
+    #         rennder 'edit'
+    #     end    
+    # end     
+        
+end  

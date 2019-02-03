608208e9764d19661ce9526d384e0446c70838d6
Sofia Rahimi <sofia.i.rahimi@gmail.com>
2019-02-03 05:39:43 +0000

    quick edit application.rb precompiling assets

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+# 
+
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
@@ -6,7 +8,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Workspace
+module TestApp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -19,8 +21,6 @@ module Workspace
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    # config/application.rb
-    config.assets.initialize_on_precompile = false
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true

57f651fbb63cd1f77d9727cd772b7098a853719f
Sofia Rahimi <sofia.i.rahimi@gmail.com>
2019-02-24 00:12:38 +0000

    complete authentication system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,6 +55,10 @@ class UsersController < ApplicationController
   end
   
   def require_same_user
+     if current_user != @user
+        flash[:danger] = "You can only edit your own account"
+        redirect_to root_path
+     end
   
   end
   

a7a5dc47ea12602a784751631d0c198e1fb2d647
Sofia Rahimi <sofia.i.rahimi@gmail.com>
2019-02-24 18:18:28 +0000

    Add admin user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,8 @@ class UsersController < ApplicationController
   end
   
   def require_same_user
-     if current_user != @user
+     if current_user != @user and !current_user.admin?
+     
         flash[:danger] = "You can only edit your own account"
         redirect_to root_path
      end
@@ -64,7 +65,7 @@ class UsersController < ApplicationController
   
   def require_admin
     if logged_in? and !current_user.admin? 
-      flash[:danger] = "Only admin users can perform that action"
+      flash[:danger] = "Only Admin users can perform that action"
       redirect_to root_path
     end
   end

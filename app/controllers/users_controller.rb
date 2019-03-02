525e0fb9c346e97e3b20566da40cc71ebfbbb0a8
Sofia Rahimi <sofia.i.rahimi@gmail.com>
2019-03-02 02:06:47 +0000

    add welcome view to correspond  welcome controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
   before_action :require_same_user, only: [:edit, :update, :destroy]
   before_action :require_admin, only: [:destroy]
+ 
   def index
     @users = User.paginate(page: params[:page], per_page: 4)
   end
@@ -34,8 +35,6 @@ class UsersController < ApplicationController
   end
   
   def show
-  # @user = User.find(params[:id])
-  # @user_articles = @user.articles.paginate(page: params[:page], per_page:5)
    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
   

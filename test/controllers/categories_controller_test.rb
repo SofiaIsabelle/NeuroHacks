9d7a46b72c6eec038e83915b6787fcee25a3c111
Sofia Rahimi <sofia.i.rahimi@gmail.com>
2019-03-14 17:46:16 +0000

    finish adding admin requirements to create categories

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -25,8 +25,8 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   
   test "should redirect create when admin not logged in" do
     assert_no_difference 'Category.count' do
-      post categories_path, params: { category: { name: "Neuroscience" } }
+        post categories_path, params: { category: { name: "Neuroscience" } }
     end
-    assert_redirected_to categories_path
+        assert_redirected_to categories_path
   end
 end
